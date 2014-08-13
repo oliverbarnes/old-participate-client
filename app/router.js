1e06d08d66ef874327eb7625a9d6c87ecd712583
Cathy + Qian <cnangini@gmail.com>
2014-08-13 17:43:47 +0300

    WIP Pirkka's solution to router.js. Make a suggestion link points to right url but is not wired up.

--- app/router.js
+++ app/router.js
@@ -9,13 +9,25 @@ Router.map(function() {
     this.route('new'); // /initiatives/new
     this.route('show', {path: '/:initiative_id'}); // /initiatives/1
     this.resource('suggestions', {path: '/:initiative_id/suggestions'}, function() {
-  });
-  this.resource('initiative', { path: '/initiatives/:initiative_id' }, function() { // /initiatives/1/
-    this.resource('suggestions', function() {
       this.route('new'); // /initiatives/1/suggestions/new
       this.route('show', {path: '/:suggestion_id'}); // /initiatives/1/suggestions/100
     });
   });
 });
+
+
+// Router.map(function() {
+//   this.resource('initiatives', function() {
+//     this.route('new'); // /initiatives/new
+//     this.route('show', {path: '/:initiative_id'}); // /initiatives/1
+//     this.resource('suggestions', {path: '/:initiative_id/suggestions'}, function() {
+//   });
+//   this.resource('initiative', { path: '/initiatives/:initiative_id' }, function() { 
+//     this.resource('suggestions', function() {
+//       this.route('new'); // /initiatives/1/suggestions/new
+//       this.route('show', {path: '/:suggestion_id'}); // /initiatives/1/suggestions/100
+//     });
+//   });
+// });
 
 export default Router;
