5a248e7017f508af0364c69a5439118ad6970548
Cathy + Qian <cnangini@gmail.com>
2014-08-13 17:18:55 +0300

    Pirkka's router.js solution. Make a suggestion link has right url but is not wired up.

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ Router.map(function() {
   this.resource('initiatives', function() {
     this.route('new'); // /initiatives/new
     this.route('show', {path: '/:initiative_id'}); // /initiatives/1
-    this.resource('suggestions', function() {
+    this.resource('suggestions', {path: '/:initiative_id/suggestions'}, function() {
       this.route('new'); // /initiatives/1/suggestions/new
       this.route('show', {path: '/:suggestion_id'}); // /initiatives/1/suggestions/100
     });
