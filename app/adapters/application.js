0be656a3cbd5fa14704ba079403c14832a13de4e
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2014-08-19 22:28:59 +0200

    Fix tests teardown conflict bug. Closes #34

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,1 +1,11 @@
-export default DS.FixtureAdapter.extend({});
+export default DS.FixtureAdapter.extend({
+  fixturesForType: function(type) {
+    var result = this._super(type);
+    if(result) {
+      result = Ember.EnumerableUtils.map(result, function(fixture) {
+        return jQuery.extend(true, {}, fixture);
+      });
+    }
+    return result;
+  }
+});
