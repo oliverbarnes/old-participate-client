1b566f485c716b47f56b2e8da78d439ed4a327bf
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2014-12-11 21:43:55 -0200

    Mocha tests fully working with ember-cli-mocha

--- tests/acceptance/posting-an-initiative-test.js
+++ tests/acceptance/posting-an-initiative-test.js
@@ -6,55 +6,65 @@ var App;
 describe('Posting an initiative', function() {
   beforeEach(function(){
     App = startApp();
+    visit('/');
+    click( $("a:contains('Start a new initiative')") );
   });
   
   afterEach(function() {
     Ember.run(App, 'destroy');
   });
 
-  it('Successfully', function(){
-    visit('/').then(function() {
-      click( $("a:contains('Start a new initiative')") ).then(function() {
-        expect($(':submit').attr('disabled')).to.equal('disabled');
-        expect(currentURL()).to.equal('/initiatives/new');
-        fillIn('div.title input', 'Public health clinic');
-        fillIn('div.description textarea', 'Allocate compensation money to create a local public health clinic');
-        fillIn('div.issue_title input', "What to do with the compensation money from the dam's impact?");
-        fillIn('div.issue_description textarea', "The contruction company in charge of the dam will pay 10 million in compensation to the local affected population. What to do with it?");
-        click('form input[type=submit]').then(function() {
-          // Ideally I'd prefer to:
-          //   expect(currentURL()).to.equal('/initiatives/' + initiativeID);
-          // but haven't found an ember equivalent of active record's Post.first.id,
-          // nor a way to get at the local store and use store.all('initiative').objectAt(0).get('id').
-          // Also can't get a record count yet
-          expect(currentPath()).to.equal('initiatives.initiative.index');
-          expect(find('.title').text()).to.equal('Public health clinic');
-          expect(find('.description').text()).to.equal('Allocate compensation money to create a local public health clinic');       
-          expect(find('.issue_title').text()).to.equal("For issue: What to do with the compensation money from the dam's impact?");
-        });
+  describe('before posting', function() {
+    it('the submit button is disabled', function() {
+      expect($(':submit').attr('disabled')).to.equal('disabled');
+    });
+
+    it('url path is /initiatives/new', function() {
+      expect(currentURL()).to.equal('/initiatives/new');
+    });
+  });
+
+  describe('when the initiative is valid', function() {
+    beforeEach(function() {
+      fillIn('div.title input', 'Public health clinic');
+      fillIn('div.description textarea', 'Allocate compensation money to create a local public health clinic');
+      fillIn('div.issue_title input', "What to do with the compensation money from the dam's impact?");
+      fillIn('div.issue_description textarea', "The contruction company in charge of the dam will pay 10 million in compensation to the local affected population. What to do with it?");      
+      click('form input[type=submit]');
+    });
+
+    it('current path is initiatives.initiative.index', function() {
+      expect(currentPath()).to.equal('initiatives.initiative.index');
+    });
+
+    it('the title is displayed', function() {
+      expect(find('.title').text()).to.equal('Public health clinic');
+    });
+
+    it('the description is displayed', function() {
+      expect(find('.description').text()).to.equal('Allocate compensation money to create a local public health clinic');             
+    });
+
+    it('the issue title is displayed', function() {
+      expect(find('.issue_title').text()).to.equal("For issue: What to do with the compensation money from the dam's impact?");      
+    });
+  });
+
+  describe('With the title missing', function(){
+    it('an error is shown next to the title field', function() {
+      expect(find('div.fieldWithErrors .error').text()).to.equal('');
+      triggerEvent('div.title input', 'blur').then(function() {
+        expect(find('div.fieldWithErrors .error').text()).to.equal("can't be blank");
+      });
+    });
+  });
+
+  describe('With the description missing', function(){
+    it('an error is shown next to the description field', function() {
+      expect(find('div.fieldWithErrors .error').text()).to.equal('');
+      triggerEvent('div.description textarea', 'blur').then(function() {
+        expect(find('div.fieldWithErrors .error').text()).to.equal("can't be blank");
       });
     });
   });
 });
-
-//   it('With the title missing', function(){
-//     visit('/').then(function() {
-//     click( $("a:contains('Start a new initiative')") ).then(function() {
-//       expect(find('div.fieldWithErrors .error').text()).to.equal('');
-//       triggerEvent('div.title input', 'blur').then(function() {
-//         expect(find('div.fieldWithErrors .error').text()).to.equal("can't be blank");
-//       });
-//     });
-//   });
-// });
-
-//   it('With the description missing', function(){
-//     visit('/').then(function() {
-//     click( $("a:contains('Start a new initiative')") ).then(function() {
-//       expect(find('div.fieldWithErrors .error').text()).to.equal('');
-//       triggerEvent('div.description textarea', 'blur').then(function() {
-//         expect(find('div.fieldWithErrors .error').text()).to.equal("can't be blank");
-//       });
-//     });
-//   });
-// });
