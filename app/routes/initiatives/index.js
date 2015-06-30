9f2ea357214b16c35fc42c6461294dcb1e0a14e0
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2015-06-29 20:20:04 -0400

    Scrap old version

--- app/routes/initiatives/index.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-var InitiativesIndexRoute = Ember.Route.extend({
-  model: function() {
-    return this.store.find('initiative');
-  }
-});
-
-export default InitiativesIndexRoute;
--- app/routes/issues/index.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-var IssuesIndexRoute = Ember.Route.extend({
-  model: function() {
-    return this.store.find('issue');
-  }
-});
-
-export default IssuesIndexRoute;
