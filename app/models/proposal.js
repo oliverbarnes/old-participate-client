4b6692f89660250ecb278bd70f78174526804f35
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2015-07-19 20:42:59 +0200

    Start on Proposal#toggleSupport

--- app/models/proposal.js
+++ app/models/proposal.js
@@ -14,6 +14,14 @@ export default Resource.extend({
     // TODO: add unit test and implement
     //
     // if proposal doesn't have a associated support with current user associated,
+    support = Support.new();
+    support.addRelationship('proposal', this.get('id'));
+    //
+    // TODO: time to introduce the current_user on the front-end
+    // ...or current_login, as we don't yet have a user (participant) model
+    // on the api
+    support.addRelationship('owner', ...)
+    this.store.createResource('supports', support);
     //   create and associate a new support resource to itself
     // else
     //   remove associated support
