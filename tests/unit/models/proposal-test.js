3c59c61eff53b527cb48895fe62c63dcda54ec26
Bill Heaton <bill@mediaartslab.com>
2015-08-04 12:10:11 -0700

    remove before/after did not work

--- tests/unit/models/proposal-test.js
+++ tests/unit/models/proposal-test.js
@@ -1,34 +1,19 @@
 /* jshint expr:true */
 import { expect } from 'chai';
-import {
-  describe,
-  it
-} from 'ember-mocha';
+import { describeModule, it } from 'ember-mocha';
 import Resource from '../../../models/proposal';
 
-// var subject;
+describeModule('model:proposal', 'ProposalResource', function() {
 
-// describeModule('model:proposal', 'Unit | Model| Proposal',{}, function() {
-describe('proposal', function() {
+  it('adds a support to it', function() {
+    Resource.prototype.container = this.container;
+    this.container.register('model:proposals', Resource, {});
 
-  // describe('#toggleSupport', function() {
-    before(function() {
-      debugger;
-      Resource.prototype.container = this.container;
-      // Use a non-standard name, i.e. pluralized instead of singular
-      this.__container__.register('model:proposalz', Resource, {});
-      subject = this.__container__.lookupFactory('model:proposalz').create();
-    });
+    //var subject = Resource.create();
+    var subject = this.container.lookupFactory('model:proposals').create();
+    //expect(this.subject().get('supports')).to.eql([]);
+    expect(subject.toggleSupport()).to.be.true;
 
-    after(function() {
-      delete Resource.prototype.container;
-    });
-
-    // describe('when proposal is not supported yet', function() {
-      it('adds a support to it', function() {
-        // expect(this.subject().get('supports')).to.eql([]);
-        expect(subject.toggleSupport()).to.be.true;
-      });
-    // });
-  // });
+    delete Resource.prototype.container;
+  });
 });
