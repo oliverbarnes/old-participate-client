a7aac5613dd9c23a1be95c3600677a9b64ba1149
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2015-08-04 18:30:01 +0100

    Use a promise object in creating the current user in the session

--- app/sessions/me.js
+++ app/sessions/me.js
@@ -1,13 +1,26 @@
 import Ember from 'ember';
 import Session from 'simple-auth/session';
 
+// export default Session.extend({
+//   me: function() {
+//     var accessToken = this.get('secure.access_token');
+//     if (!Ember.isEmpty(accessToken)) {
+//       let self = this;
+//       return this.container.lookup('service:me').find({}).then((me) => {
+//         self.set('me', me);
+//       });
+//     }
+//   }.property('secure.access_token')
+// });
+
+var PromiseObject = Ember.ObjectProxy.extend(Ember.PromiseProxyMixin);
+
 export default Session.extend({
   me: function() {
     var accessToken = this.get('secure.access_token');
     if (!Ember.isEmpty(accessToken)) {
-      let self = this;
-      return this.container.lookup('service:me').find({}).then((me) => {
-        self.set('me', me);
+      return PromiseObject.create({
+        promise: this.container.lookup('service:me').find({})
       });
     }
   }.property('secure.access_token')
