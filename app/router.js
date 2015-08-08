dae732b2492c97d20bce71b17b50a1cf3298cd9d
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2015-08-08 18:23:53 +0100

    Login page at root, better styling

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('login');
+  this.route('login', { path: '/' });
   // this.route('activity');
   this.route('dashboard');
   this.route('proposals', function() {
