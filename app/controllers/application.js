7c6b066618695fbf90feebdb680ed74ce08543cd
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2015-08-10 21:26:08 +0100

    Hide navigation on login page when user is logged in

---
+++ app/controllers/application.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isLoginRoute: Ember.computed('currentPath', function() {
+    return this.get('currentPath') === 'login'
+  })
+});
