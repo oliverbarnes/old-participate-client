6aec36361ef58a63c5b132bccc62c4489a40faea
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2015-10-24 22:49:06 -0200

    Use /internal for authenticated routes

--- app/routes/internal.js
+++ app/routes/internal.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
-  model: function() {
-    return this.store.find('me', { singleton: true });
-  }
-});
+export default Ember.Route.extend(AuthenticatedRouteMixin);
--- app/routes/internal/proposals/new.js
+++ app/routes/internal/proposals/new.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend({
   model() {
     return this.container.lookup('model:proposals').create({
       isNew: true
