171b48e64368134758282932386a9dee75d327e2
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2016-01-25 19:00:45 -0200

    /proposal-list

---
+++ app/models/proposal.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+const { attr } = DS;
+
+export default DS.Model.extend({
+  title: attr('string'),
+  body:  attr('string'),
+  'support-count': attr('string')
+});
--- app/routes/proposal-list.js
+++ app/routes/proposal-list.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model() {
+    return this.get('store').findAll('proposal');
+  }
 });
