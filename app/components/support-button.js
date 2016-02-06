d7c4b83197b82fbed9ab646a11eb6090f7a50dfc
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2016-02-06 00:02:00 -0200

    Move support button text to a computed property and WIP getting mouse overs to show correct changed text

--- app/components/support-button.js
+++ app/components/support-button.js
@@ -13,7 +13,26 @@ export default Ember.Component.extend({
     }
   },
 
+  // TODO: figure out who to handle button text changes.
+  // clicks between mouseEnter and mouseLeave complicate things
+  // 
+  // mouseEnter() {
+  //   if(this.get('text') == 'Backing') {
+  //     this.set('text', 'Remove support');
+  //   }
+  // },
+
+  // mouseLeave() {
+  //   if(this.get('text') == 'Remove support') {
+  //     this.set('text', this.get('originalText'));
+  //   }
+  // },
+
   disabled: computed('supportSwitch', 'proposal', function() {
     return this.get('supportSwitch').disabled(this.get('proposal')) ? 'disabled' : '';
   }),
+
+  text: computed('proposal.backedByMe', function() {
+    return this.get('proposal.backedByMe') ? 'Backing' : 'Add your support';
+  })
 });
